f7952b46dfbc732ccea937c93af44be6cea93e56
HOLYDNA <137703704+HOLYDNA@users.noreply.github.com>
2023-07-06 00:04:24 -0400

    Update constants.py

--- program/constants.py
+++ program/constants.py
@@ -5,7 +5,7 @@ from decouple import config
 MODE = "DEVELOPMENT"
 
 # Close all open positions and orders
-ABORT_ALL_POSITIONS = True
+ABORT_ALL_POSITIONS = False
 
 # Find Cointegrated Pairs
 FIND_COINTEGRATED = True
@@ -25,8 +25,8 @@ WINDOW = 21
 # Thresholds - Opening
 MAX_HALF_LIFE = 24
 ZSCORE_THRESH = 1.5
-USD_PER_TRADE = 3
-USD_MIN_COLLATERAL = 50
+USD_PER_TRADE = 5
+USD_MIN_COLLATERAL = 100
 
 # Thresholds - Closing
 CLOSE_AT_ZSCORE_CROSS = True
